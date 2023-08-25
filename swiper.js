8f8843a28c0d9957d0e8ed0622819066d9f13687
aytac <qehremanova.aytac2003@gmail.com>
2023-08-25 17:56:34 +0400

    trainers sectionu elave olundu

--- swiper.js
+++ swiper.js
@@ -10,3 +10,27 @@ const bannerSwiper = new Swiper(".banner-swiper", {
       clickable: true,
     }
   });
+
+
+  const trainersSwiper = new Swiper(".trainers-swiper", {
+    loop: true,
+    autoplay: {
+      delay: 2000,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    slidesPerView: 1,
+    breakpoints:{
+      700:{
+        slidesPerView:3,
+        spaceBetween:30,
+      },
+
+      1024:{
+        slidesPerView:4,
+        spaceBetween:30,
+      }
+    }
+  });
