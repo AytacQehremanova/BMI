050b12caac561df1567b5d56d46eefa63593eba4
aytac <qehremanova.aytac2003@gmail.com>
2023-08-28 17:18:57 +0400

    events ve news sectionlari elave olundu

--- swiper.js
+++ swiper.js
@@ -34,3 +34,28 @@ const bannerSwiper = new Swiper(".banner-swiper", {
       }
     }
   });
+
+
+
+  const newsSwiper = new Swiper(".news-swiper", {
+    loop: true,
+    autoplay: {
+      delay: 2000,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    slidesPerView: 1,
+    breakpoints:{
+      700:{
+        slidesPerView:3,
+        spaceBetween:30,
+      },
+
+      1024:{
+        slidesPerView:3,
+        spaceBetween:30,
+      }
+    }
+  });
